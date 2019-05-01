68580f6c75e064fa08eb04c8c9a7f9862ba9a828
Jacob Weinbren <jacobweinbren@gmail.com>
2019-05-01 12:38:45 +0100

    Added main sites content

--- README.md
+++ README.md
@@ -4,11 +4,20 @@ Path to 326...give or take depending on Sinn Fein seats, the Speaker and Deputy 
 A project by Dan W and Jacob W
 
 ## Sources
+- https://github.com/calvinmetcalf/shapefile-js
+- https://picturepan2.github.io/spectre/getting-started/installation.html
+https://iconmonstr.com/paintbrush-2-png/
+- https://leafletjs.com/examples/geojson/
+- https://data.gov.uk/dataset/b77fcedb-4792-4de4-935f-4f344ed4c2c6/general-election-results-2017
+- https://github.com/evanplaice/jquery-csv/
+- https://en.wikipedia.org/wiki/Wikipedia:Index_of_United_Kingdom_political_parties_meta_attributes
+
 For the Hex Map
-https://github.com/timcraft/electionmap
+- https://github.com/timcraft/electionmap
 
 For the UK Geographically Accurate Map (depending on your opinion of the mercator projection)
-https://geoportal.statistics.gov.uk/datasets/westminster-parliamentary-constituencies-december-2017-generalised-clipped-boundaries-in-the-uk?geometry=-10.015%2C54.057%2C-2.104%2C55.17
+- https://geoportal.statistics.gov.uk/datasets/westminster-parliamentary-constituencies-december-2017-generalised-clipped-boundaries-in-the-uk?geometry=-8.114%2C51.915%2C7.706%2C54.224
+- https://github.com/calvinmetcalf/leaflet-ajax
 
 ## Parties
 Not sure what order to do these without being biased. So for now it's being done by the number of seats at the moment, including defections. For the sake of inpartiality and openess, the choices of party will be 
---
+++ js/main.js
@@ -0,0 +1,135 @@
+map = L.map('mapid', {
+    zoomSnap: 0.2,
+
+}).setView([54, -2], 6);
+
+$.ajax({
+    url: "results.csv",
+    async: false,
+    success: function(data) {
+        results = $.csv.toArrays(data);
+    }
+});
+
+parties = {
+    "C": "Conservative",
+    "DUP": "Democratic Unionist Party",
+    "Green": "Green Party",
+    "Ind": "Independent",
+    "Lab": "Labour",
+    "LD": "Liberal Democrats",
+    "PC": "Plaid Cymru",
+    "SF": "Sinn Fein",
+    "SNP": "SNP"
+}
+
+colours = {
+    "C": "#0087DC",
+    "DUP": "#D46A4C",
+    "Green": "#6AB023",
+    "Ind": "darkgrey",
+    "Lab": "#DC241f",
+    "LD": "#FAA61A",
+    "PC": "#008142",
+    "SF": "#326760",
+    "SNP": "#FEF987",
+    "Brex": "#12B6CF",
+    "CHUK": "#ffffff"
+}
+
+seats = {
+    "C": 318,
+    "Lab": 262,
+    "SNP": 35,
+    "LD": 12,
+    "DUP": 10,
+    "SF": 7,
+    "PC": 4,
+    "Green": 1,
+    "Ind": 2
+}
+
+for (colour in colours) {
+    $("#" + colour).css('background-color', colours[colour])
+    $("#c-" + colour).css('background-color', colours[colour])
+}
+
+function gains() {
+    constituencies.eachLayer(function(layer) {
+        layer.setStyle({
+            fillOpacity: 0.1,
+            opacity: 0.1
+        });
+        for (i = 0; i < results.length; i++) {
+            if (layer.constituency == results[i][0]) {
+                if (layer.party != results[i][1]) {
+                    layer.setStyle({
+                        fillOpacity: 1,
+                        opacity: 1
+                    });
+                }
+            }
+        }
+    });
+}
+
+function regular() {
+    constituencies.eachLayer(function(layer) {
+        layer.setStyle({
+            fillOpacity: 1,
+            opacity: 1
+        });
+    });
+}
+
+
+
+function getResult(constituency) {
+    for (i = 0; i < results.length; i++) {
+        if (results[i][0] == constituency) {
+            party = results[i][1]
+            return party
+        }
+    }
+}
+
+$('#draw div').click(function(event) {
+    mode = 'draw';
+    party_id = event.target.id.split('-')[1]
+    $('#mode').html('Mode: <font color="' + colours[party_id] + '">Draw</font>')
+});
+
+shp("westminster.zip").then(function(geojson) {
+    console.log(geojson)
+    constituencies = L.geoJSON(geojson, {
+        onEachFeature: function(feature, layer) {
+            constituency = feature.properties.pcon17nm
+            layer.party = getResult(constituency)
+            layer.constituency = constituency
+            layer.setStyle({
+                fillColor: colours[getResult(constituency)]
+
+            });
+
+            layer.on('mouseover', function() {
+                constituency = feature.properties.pcon17nm
+                party_name = parties[getResult(constituency)]
+                $('#name').html("<font color='" + colours[getResult(constituency)] + "'>" + party_name + "</font><br>" + constituency)
+            });
+
+            layer.on('click', function() {
+                if (mode == 'draw') {
+                    layer.setStyle({
+                        fillColor: colours[party_id]
+                    });
+                    layer.party = party_id
+                    feature.properties
+                }
+            });
+        },
+        weight: 0.3,
+        color: '#000000',
+        fillOpacity: 1,
+        smoothFactor: 0.9
+    }).addTo(map);
+});
